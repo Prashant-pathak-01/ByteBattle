32a6056cf4bc359d8a486ea74562d8cf38b71321
prashant-pathak-01 <prashantpathak6395@gmail.com>
2024-07-12 21:14:08 +0530

    Auth page updated

--- frontend/src/Components/HomePage/Homepage.js
+++ frontend/src/Components/HomePage/Homepage.js
@@ -39,7 +39,6 @@ function Home() {
               today and build a solid foundation for your future in tech. Turn
               your coding aspirations into reality with ByteBattle!
             </p>
-
             <Link to="/sheet">
               <button class="relative inline-flex w-40 items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
                 <span class="relative text-lg w-40 px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
